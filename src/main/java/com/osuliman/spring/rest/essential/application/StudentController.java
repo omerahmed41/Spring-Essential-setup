f028106ddd190195620e7be030e5d31fd9d7e53f
omerSuliman <omerahmed41@gmail.com>
2023-10-01 00:50:03 +0200

    - make all requests open.

--- src/main/java/com/osuliman/spring/rest/essential/application/StudentController.java
+++ src/main/java/com/osuliman/spring/rest/essential/application/StudentController.java
@@ -3,6 +3,7 @@ package com.osuliman.spring.rest.essential.application;
 import com.osuliman.spring.rest.essential.domain.Student;
 import com.osuliman.spring.rest.essential.domain.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Secured("ROLE_ADMIN")
 @RequestMapping(path = "/api/v1")
 public class StudentController {
     private final StudentService studentService;
--- src/main/java/com/osuliman/spring/rest/essential/application/security/DemoSecurityConfig.java
+++ src/main/java/com/osuliman/spring/rest/essential/application/security/DemoSecurityConfig.java
@@ -2,7 +2,6 @@ package com.osuliman.spring.rest.essential.application.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -59,21 +58,7 @@ public class DemoSecurityConfig {
         http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers(HttpMethod.GET,
-                                        "/").hasRole("MANAGER")
-                                .requestMatchers(HttpMethod.GET,
-                                        "/users").hasRole("MANAGER")
-                                .requestMatchers(HttpMethod.GET,
-                                        "/swagger**").hasRole("MANAGER")
-                                .requestMatchers(HttpMethod.GET,
-                                        "/api/employees/**").hasRole("EMPLOYEE")
-                                .requestMatchers(HttpMethod.POST,
-                                        "/api/employees").hasRole("MANAGER")
-                                .requestMatchers(HttpMethod.PUT,
-                                        "/api/employees").hasRole("MANAGER")
-                                .requestMatchers(HttpMethod.DELETE,
-                                        "/api/employees/**").hasRole("ADMIN")
-                                .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
